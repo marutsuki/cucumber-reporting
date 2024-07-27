bf31064d8e25ca91d6e952f7d8138fbf4c1c7046
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-27 21:22:52 +1000

    processFeature function

---
+++ src/processing/process.ts
@@ -0,0 +1,8 @@
+import * as fs from 'fs';
+
+export default function processFeature(filePath: string): Feature {
+    console.info('Processing...');
+    const feature: Feature = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    console.debug('Processed feature:', filePath);
+    return feature;
+}
