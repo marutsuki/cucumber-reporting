b7776df466660dd298fd56aff67fb70d992a8c8e
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-27 21:10:46 +1000

    Types for cucumber report

---
+++ src/processing/types.d.ts
@@ -0,0 +1,44 @@
+type Match = {
+    location: string;
+};
+
+type Result = {
+    status: string;
+    duration: number;
+    error_message?: string;
+};
+
+type Step = {
+    keyword: string;
+    line: number;
+    name: string;
+    match?: Match;
+    result?: Result;
+};
+
+type Tag = {
+    name: string;
+    line: number;
+};
+
+type Scenario = {
+    description: string;
+    id: string;
+    keyword: string;
+    line: number;
+    name: string;
+    steps: Step[];
+    tags: Tag[];
+    type: string;
+};
+
+type Feature = {
+    id: string;
+    line: number;
+    description: string;
+    elements: Scenario[];
+    keyword: string;
+    name: string;
+    tags: Tag[];
+    uri: string;
+};
