a4adf31b4a0cc51596635a99664a1e90b9ed2d5d
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-28 18:27:34 +1000

    Update shell script to use getopt

--- run.sh
+++ run.sh
@@ -1,19 +1,29 @@
+#!/bin/bash
 
 OUTDIR='./out'
 PROJDIR=''
 THEME=''
-if [ "$1" != "" ]; then
-    if [ "$2" != "" ]; then
-        OUTDIR=$2
-    fi
-    if [ "$3" != "" ]; then
-        PROJDIR=$3
-    fi
-    if [ "$4" != "" ]; then
-        THEME=$4
-    fi
-    node_modules/.bin/tailwindcss -i ./src/ui/globals.css -o $OUTDIR/globals.css
+
+while getopts 'o:p:t:' flag; do
+    case "${flag}" in
+        o) OUTDIR="${OPTARG}" ;;
+        p) PROJDIR="${OPTARG}" ;;
+        t) THEME="${OPTARG}" ;;
+        *) echo "Usage: $0 [-o <output directory>] [-p <project directory>] [-t <theme>]" >&2
+        exit 1 ;;
+    esac
+done
+
+ARG1=${@:$OPTIND:1}
+
+if [ $ARG1 != "" ]; then
     cp ./script.js $OUTDIR/script.js
-    node dist/index.js $1 $OUTDIR $PROJDIR $THEME
+    if [ -x "$basedir/node" ]; then
+        exec "$basedir/node_modules/.bin/tailwindcss" -i ./src/ui/globals.css -o $OUTDIR/globals.css
+        exec "$basedir/node"  dist/index.js $ARG1 $OUTDIR $PROJDIR $THEME
+    else 
+        node_modules/.bin/tailwindcss -i ./src/ui/globals.css -o $OUTDIR/globals.css
+        node dist/index.js $ARG1 $OUTDIR $PROJDIR $THEME
+    fi
 fi
 
