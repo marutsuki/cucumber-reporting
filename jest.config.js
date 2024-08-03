f4b2bc7047d9d748bf62d8d5241f8e4fbfddad9a
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-08-03 17:12:08 +1000

    Adjusted jest config to include node_modules

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
+  moduleDirectories: [
+    "node_modules"
+  ]
 };
